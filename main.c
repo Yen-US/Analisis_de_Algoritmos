9ab314b7a40b9859aa8c4169d0b026b35588c901
ashleyvasquez <c.vasquezashley@gmail.com>
2019-10-03 13:35:12 -0600

    Se agrega el código de SelectionSort

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
-#include <stdio.h>
+#include "time.h"
+#include "Selection_Sort/SelectionSort.c"
 
 int main() {
-    printf("Hello, World!\n");
+//SelectionSort:
+    int arr[] = {11, 25, 76,43,87};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    float startTime = (float) clock() / CLOCKS_PER_SEC;
+    selectionSort_algorithms(arr, n);
+    float endTime = (float) clock() / CLOCKS_PER_SEC;
+    float timeElapsed = endTime - startTime;
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    printf("El tiempo que tardó en ordenarlo fue: %f ", timeElapsed);
     return 0;
+//Termina SelectionSort
 }
