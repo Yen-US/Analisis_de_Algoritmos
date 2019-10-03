357c072b382f7c730d0f61ae417f9ac62c35ca13
yenus <yen21000@gmail.com>
2019-10-03 15:32:08 -0600

    Se añade el código del Bubble Sort

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include "time.h"
 #include "Selection_Sort/SelectionSort.c"
+#include "Insertion_Sort/InsertionSort.c"
+#include "Bubble_Sort/BubbleSort.c"
 
 int main() {
+
 //SelectionSort:
     int arr[] = {11, 25, 76,43,87};
     int n = sizeof(arr)/sizeof(arr[0]);
@@ -12,6 +15,21 @@ int main() {
     printf("Sorted array: \n");
     printArray(arr, n);
     printf("El tiempo que tardó en ordenarlo fue: %f ", timeElapsed);
-    return 0;
 //Termina SelectionSort
+
+//Bubble Sort
+    int arrBub[] = {64, 34, 25, 600, 12, 22, 11, 90};
+    int nBub = sizeof(arrBub)/sizeof(arrBub[0]);
+    float startTimeBub = (float) clock() / CLOCKS_PER_SEC;
+    bubbleSort(arrBub, nBub);
+    float endTimeBub = (float) clock() / CLOCKS_PER_SEC;
+    float timeElapsedBub = endTimeBub - startTimeBub;
+    printf("Sorted array: \n");
+    printArray(arrBub, nBub);
+    printf("El tiempo que tardó en ordenarlo fue: %f ", timeElapsedBub);
+//Fin Bubble Sort
+
+
+
+    return 0;
 }
